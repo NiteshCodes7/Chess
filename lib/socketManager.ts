82afd7976d4771200743349c8d05deab539fa5a1
NiteshCodes7 <workwithnites@gmail.com>
2026-04-04 02:41:44 +0530

    Updated socketManager

--- lib/socketManager.ts
+++ lib/socketManager.ts
@@ -1,25 +1,24 @@
 import { getSocket } from './socket';
-import { getChatSocket } from './chatSocket';
 import { getPresenceSocket } from './presenceSocket';
 
 export function connectAllSockets() {
   const token =
     typeof window !== 'undefined' ? localStorage.getItem('wsToken') : null;
 
-  [getSocket(), getChatSocket(), getPresenceSocket()].forEach((s) => {
+  [getSocket(), getPresenceSocket()].forEach((s) => {
     s.auth = { wsToken: token };
     if (!s.connected) s.connect();
   });
 }
 
 export function disconnectAllSockets() {
-  [getSocket(), getChatSocket(), getPresenceSocket()].forEach((s) =>
+  [getSocket(), getPresenceSocket()].forEach((s) =>
     s.disconnect(),
   );
 }
 
 export function updateAllSocketAuth(wsToken: string) {
-  [getSocket(), getChatSocket(), getPresenceSocket()].forEach((s) => {
+  [getSocket(), getPresenceSocket()].forEach((s) => {
     s.auth = { wsToken };
     if (!s.connected) s.connect();
   });
